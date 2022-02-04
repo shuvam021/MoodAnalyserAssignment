75ffcef8238ea1aca523d6313449b0cda7e05cdc
Shuvam Das <repository.shuvam@gmail.com>
2022-02-04 21:23:55 +0530

    initial commit

---
+++ MoodAnalyserAssignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoodAnalyserAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mood Analyser Program!");
+        }
+    }
+}
